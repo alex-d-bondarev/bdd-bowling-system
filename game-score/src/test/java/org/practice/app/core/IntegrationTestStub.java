134fde9a88c31ff7c0aba510602cd02eab35f0cc
Alex <alex.d.bondarev@gmail.com>
2020-03-24 23:39:04 +0200

    ensure that build stops docker after integration tests

---
+++ game-score/src/test/java/org/practice/app/core/IntegrationTestStub.java
@@ -0,0 +1,12 @@
+package org.practice.app.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegrationTestStub {
+
+    @Test
+    public void fail(){
+        Assert.fail();
+    }
+}
